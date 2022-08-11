58897da2fa5957f7c17fb91e913e2ffce402729d
Todd Pangilinan <pang.todd@gmail.com>
2022-08-11 14:57:31 -0700

    established initial CoffeeControl.js file.

---
+++ src/components/CoffeeControl.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import NewCoffeeForm from './NewCoffeeForm';
+import CoffeeList from './CoffeeList';
+
+class CoffeeControl extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render(){
+    return (
+      <React.Fragment>
+      </React.Fragment>
+    );
+  }
+
+}
+
+export default CoffeeControl;
