4a4d18e23a3b570ae2c8e253a02ee08580db64a9
Todd Pangilinan <pang.todd@gmail.com>
2022-08-13 18:40:46 -0700

    updated header to specify this app is for employees.

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,8 +2,12 @@ import React from "react";
 import logo from "./../img/coffeeCommandLogo.png";
 
 function Header(){
-  return (   
-    <img src= {logo} alt="illustration of sci-fi mission control technicians"/>
+
+  return (
+    <React.Fragment>   
+      <img src= {logo} alt="illustration of sci-fi mission control technicians"/>
+      <h2>Coffee technician inventory interface</h2>
+    </React.Fragment>  
   );
 }
 
